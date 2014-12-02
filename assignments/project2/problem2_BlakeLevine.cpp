c2f4d0fc979091bf1fd80e97e1ad4ca6d0227ab8
BraveLilToaster <BraveLilToaster@users.noreply.github.com>
2014-12-02 17:42:17 -0500

    Update problem2_BlakeLevine.cpp

--- assignments/project2/problem2_BlakeLevine.cpp
+++ assignments/project2/problem2_BlakeLevine.cpp
@@ -1,139 +1,163 @@
 #include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-void boardState(char sideBoard[][4], int board[][4]); 
+void boardState(char sideBoard[][4], int board[][4]);
 void shuffle(int board[][4], char sideBoard[][4]);
 void flip(int& match, char sideBoard[][4], int board[][4]);
 void clr();
 
 int main() {
-    int board[4][4];
-    char sideBoard[4][4];
-    int match = 0;
+	int board[4][4];
+	char sideBoard[4][4];
+	int match = 0;
 
-    shuffle(board, sideBoard);
+	shuffle(board, sideBoard);
 
-    do {
-    //menu
-    boardState(sideBoard, board);
-    //enter spots to flip over
-    flip(match, sideBoard, board);
-    }
-    while(match<8) ;
+	do {
+		//menu
+		boardState(sideBoard, board);
+		//enter spots to flip over
+		flip(match, sideBoard, board);
+	} while (match<8);
 
-    return 0;
+	return 0;
 }
 
 void flip(int& match, char sideBoard[][4], int board[][4]) {
-    int r1=0, r2=0, c1=0, c2=0;
-    cout << "Enter the coordinates of the first card seperated by a space: ";
-    cin >> r1 >> c1;
-    //sideBoard[r1-1][c1-1] = static_cast<char>(board[r1-1][c1-1]);
-    sideBoard[r1-1][c1-1] = board[r1-1][c1-1];
-    boardState(sideBoard, board);
+	int r1 = 0, r2 = 0, c1 = 0, c2 = 0;
+	char cont;
+	cout << "Enter the coordinates of the first card seperated by a space: ";
+	cin >> r1 >> c1;
+	sideBoard[r1-1][c1-1] = static_cast<char>((board[r1-1][c1-1])+ 48);
+	
+	boardState(sideBoard, board);
 
-    cout << "Enter the coordinates of the second card seperated by a space: ";
-    cin >> r2 >> c2;
-    //sideBoard[r2-1][c2-1] = static_cast<char>(board[r2-1][c2-1]);
-    sideBoard[r2-1][c2-1] = board[r2-1][c2-1];
+	cout << "Enter the coordinates of the second card seperated by a space: ";
+	cin >> r2 >> c2;
+	sideBoard[r2-1][c2-1] = static_cast<char>((board[r2-1][c2-1])+ 48);
+	boardState(sideBoard, board);
 
-    if(board[r1-1][c1-1] == board[r2-1][c2-1]){
-        cout << "Hooray!!! It's a match."<< endl;
-        match++;
-        cout << "Hit ENTER to continue";
+	if (board[r1-1][c1-1] == board[r2-1][c2-1]){
+		cout << "Hooray!!! It's a match." << endl;
+		match++;
+		cout << "Continue? (y/n)";
+		going:
+		cin >> cont;
+		if (cont == 'y'){
+			
+		}
+		else if (cont == 'n'){
+			return;
+		}
+		else{
+			cout << "Invalid input. Continue? (y/n)" << endl;
+			goto going;
+		}
+	}
+	else if (board[r1 - 1][c1 - 1] != board[r2 -1][c2 - 1]){
+		boardState(sideBoard, board);
+		cout << "Oops. Try again." << endl;
 
-        cin.ignore(100, '\n');
-    }
-    else if(board[r1-1][c1-1] != board[r2-1][c2-1]){
-        boardState(sideBoard, board);
-        cout << "Oops. Try again."<< endl;
-        cout << "Hit ENTER to continue";
+		cout << "Continue? (y/n)";
+		gong:
+		cin >> cont;
+		if (cont == 'y'){
 
-        cin.ignore(100, '\n');
-        sideBoard[r1-1][c1-1] = '*';
-        sideBoard[r2-1][c2-1] = '*';
-    }
+		}
+		else if (cont == 'n'){
+			return;
+		}
+		else{
+			cout << "Invalid input. Continue? (y/n)" << endl;
+			goto gong;
+		}
+		sideBoard[r1 - 1][c1 - 1] = '*';
+		sideBoard[r2 - 1][c2 - 1] = '*';
+	}
 }
 
 //displays game board
 void boardState(char sideBoard[][4], int board[][4]) {
-        //clear window
-        clr();
-        //display board
-        
-        cout << setfill('-') << "    1   2   3   4  " << endl;
-        cout << "  " << setfill('-') << setw(17) << "" << endl;
-        cout << setfill(' ') << "1 | " << sideBoard[0][0] << " | " << sideBoard[0][1] << " | " << sideBoard[0][2] << " | " << sideBoard[0][3] << " |" << endl;
-        cout << "  " << setfill('-') << setw(17) << "" << endl;
-        cout << setfill(' ') << "2 | " << sideBoard[1][0] << " | " << sideBoard[1][1] << " | " << sideBoard[1][2] << " | " << sideBoard[1][3] << " |" << endl;
-        cout << "  " << setfill('-') << setw(17) << "" << endl;
-        cout << setfill(' ') << "3 | " << sideBoard[2][0] << " | " << sideBoard[2][1] << " | " << sideBoard[2][2] << " | " << sideBoard[2][3] << " |" << endl;
-        cout << "  " << setfill('-') << setw(17) << "" << endl;
-        cout << setfill(' ') << "4 | " << sideBoard[3][0] << " | " << sideBoard[3][1] << " | " << sideBoard[3][2] << " | " << sideBoard[3][3] << " |" << endl;
-        cout << "  " << setfill('-') << setw(17) << "" << endl;
+	//clear window
+	clr();
+	//display board
+
+	cout << setfill('-') << "    1   2   3   4  " << endl;
+	cout << "  " << setfill('-') << setw(17) << "" << endl;
+	cout << setfill(' ') << "1 | " << sideBoard[0][0] << " | " << sideBoard[0][1] << " | " << sideBoard[0][2] << " | " << sideBoard[0][3] << " |" << endl;
+	cout << "  " << setfill('-') << setw(17) << "" << endl;
+	cout << setfill(' ') << "2 | " << sideBoard[1][0] << " | " << sideBoard[1][1] << " | " << sideBoard[1][2] << " | " << sideBoard[1][3] << " |" << endl;
+	cout << "  " << setfill('-') << setw(17) << "" << endl;
+	cout << setfill(' ') << "3 | " << sideBoard[2][0] << " | " << sideBoard[2][1] << " | " << sideBoard[2][2] << " | " << sideBoard[2][3] << " |" << endl;
+	cout << "  " << setfill('-') << setw(17) << "" << endl;
+	cout << setfill(' ') << "4 | " << sideBoard[3][0] << " | " << sideBoard[3][1] << " | " << sideBoard[3][2] << " | " << sideBoard[3][3] << " |" << endl;
+	cout << "  " << setfill('-') << setw(17) << "" << endl;
 }
 
 void shuffle(int board[][4], char sideBoard[][4]) {
-    int card = 0;
-    int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0, c7 = 0, c8 = 0;
-    srand( time( NULL ) );  //  using the time seed from srand explanation 
+	int card = 0;
+	int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0, c7 = 0, c8 = 0;
+	srand(time(NULL));  //  using the time seed from srand explanation 
 
-    for(int i=0; i < 4; i++){
-        for(int j =0; j < 4; j++){
-            sideBoard[i][j] = '*';
-        }
-    }
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 4; j++){
+			sideBoard[i][j] = '*';
+		}
+	}
 
-    for(int i=0; i < 4; i++){
-        for(int j =0; j < 4; j++){
-            check:
-            card = rand()%8+1;
-            if(card == 1 && c1 < 2){
-                c1++;
-                board[i][j] = card;
-            }
-            else if(card == 2 && c2 < 2){
-                c2++;
-                board[i][j] = card;
-            }
-            else if(card == 3 && c3 < 2){
-                c3++;
-                board[i][j] = card;
-            }
-            else if(card == 4 && c4 < 2){
-                c4++;
-                board[i][j] = card;
-            }
-            else if(card == 5 && c5 < 2){
-                c5++;
-                board[i][j] = card;
-            }
-            else if(card == 6 && c6 < 2){
-                c6++;
-                board[i][j] = card;
-            }
-            else if(card == 7 && c7 < 2){
-                c7++;
-                board[i][j] = card;
-            }
-            else if(card == 8 && c8 < 2){
-                c8++;
-                board[i][j] = card;
-            }
-            else{
-                goto check;
-            }
-        }
-    }
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 4; j++){
+		check:
+			card = rand() % 8 + 1;
+			if (card == 1 && c1 < 2){
+				c1++;
+				board[i][j] = card;
+			}
+			else if (card == 2 && c2 < 2){
+				c2++;
+				board[i][j] = card;
+			}
+			else if (card == 3 && c3 < 2){
+				c3++;
+				board[i][j] = card;
+			}
+			else if (card == 4 && c4 < 2){
+				c4++;
+				board[i][j] = card;
+			}
+			else if (card == 5 && c5 < 2){
+				c5++;
+				board[i][j] = card;
+			}
+			else if (card == 6 && c6 < 2){
+				c6++;
+				board[i][j] = card;
+			}
+			else if (card == 7 && c7 < 2){
+				c7++;
+				board[i][j] = card;
+			}
+			else if (card == 8 && c8 < 2){
+				c8++;
+				board[i][j] = card;
+			}
+			else{
+				goto check;
+			}
+		}
+	}
 }
 
 void clr(){
-    #ifdef WIN32
-        system ("cls");
+#ifdef WIN32
+	system("cls");
 
-    #else 
-        // Assume UNIX System
-        system ("clear");
-    #endif
+#else 
+	// Assume UNIX System
+	system("clear");
+#endif
 
 }
